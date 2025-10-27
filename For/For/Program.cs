74e222475563491cfdd51d132e417559541b28e2
Coorv <119966974+Coorv@users.noreply.github.com>
2025-10-27 11:39:41 +0200

    iseseisev ülesanne

--- For/For/Program.cs
+++ For/For/Program.cs
@@ -8,19 +8,57 @@
 //    Console.Beep();
 //}
 
-string[] objectArray = new string[5];
-int objectCount = 0;
-string temp = "no name";
-while (objectCount < 5)
+//string[] objectArray = new string[5]; // array, massiiv mitmest elemendist
+//int objectCount = 0; // loed kui palju kordi küsida
+//string temp = "no name"; // muutuja mis hetkel on "tühi"
+//while (objectCount < 5) // while tsükkel mis küsib nimesid
+//{
+//    Console.WriteLine("Sisesta järgmine lemmikloomanimi:");
+//    temp = Console.ReadLine(); // temp muutujasse väärtuse (mida me tahame) sisestamine
+//    objectArray[objectCount] = temp; // massiivi, asukohal objectCount asetame ajutisest muutujast väärtuse
+//    objectCount++; // tsükli inkrementatsioon
+//}
+//Console.WriteLine("Oled sisestanud järgmised objektid"); // sõnum
+//for (int i = 0; i < objectArray.Length; i++)
+//{
+//    Console.WriteLine($"{i + 1}. element on: {objectArray[i]}");
+//    // kuvame välja reanumbri koos vastava elemendiga, muutuja i abil
+//}
+//Console.ReadLine(); // peatame programmi klahvisisestuse taha
+
+// ülesanne
+
+Console.WriteLine("Mis on sinu kasutaja nimi?");
+string kasutajanimi = Console.ReadLine();
+
+if (kasutajanimi != "kris")
 {
-    Console.WriteLine("Sisesta järgmine lemmikloomanimi:");
-    temp = Console.ReadLine();
-    objectArray[objectCount] =temp;
-    objectCount++;
+    do
+    {
+        Console.WriteLine("Sa ei ole registreeritud");
+    } while (kasutajanimi != "kris");
 }
-Console.WriteLine("Oled sisestanud järgmised objektid");
-for (int i = 0; i < objectArray.Length; i++)
+else
 {
-    Console.WriteLine($"{i+1}. element on: {objectArray[i]}");
+    Console.WriteLine("Mis on sinu Parool?");
 }
-Console.ReadLine();
+string parool = Console.ReadLine(); ;
+if (parool != "1234")
+{
+    Console.WriteLine("See on vale parool");
+}
+
+string[] nimed = new string[3];
+int sisestustearv = 0;
+string sisestused = "";
+while (sisestustearv < 3)
+{
+    Console.WriteLine("Palun sisesta oma  lemmikloomade nimed");
+    nimed[sisestustearv] = Console.ReadLine();
+    sisestustearv++;
+}
+Console.WriteLine($"Sa oled sisestanud{sisestustearv}");
+Console.WriteLine("Milline on sinu lemmikuim?");
+string lemmiknimi = Console.ReadLine();
+lemmiknimi = nimed[0];
+Console.WriteLine($"Sinu lemmikuim on {lemmiknimi}, see oli {nimed[0]} nimi");
