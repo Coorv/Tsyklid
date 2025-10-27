4ad8d99791608601fe0d86f8554066f4689da0f9
Coorv <119966974+Coorv@users.noreply.github.com>
2025-10-27 09:16:18 +0200

    For tsükkel

---
+++ For/For/Program.cs
@@ -0,0 +1,26 @@
+﻿//Console.WriteLine("For tsükkel");
+
+//int kogus = 5;
+
+//for (int i = 0; i < kogus; i++)
+//{
+//    Console.WriteLine("0");
+//    Console.Beep();
+//}
+
+string[] objectArray = new string[5];
+int objectCount = 0;
+string temp = "no name";
+while (objectCount < 5)
+{
+    Console.WriteLine("Sisesta järgmine lemmikloomanimi:");
+    temp = Console.ReadLine();
+    objectArray[objectCount] =temp;
+    objectCount++;
+}
+Console.WriteLine("Oled sisestanud järgmised objektid");
+for (int i = 0; i < objectArray.Length; i++)
+{
+    Console.WriteLine($"{i+1}. element on: {objectArray[i]}");
+}
+Console.ReadLine();
