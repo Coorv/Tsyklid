3b493f309e88422f8163f8f113223bf76a352a3f
Coorv <119966974+Coorv@users.noreply.github.com>
2025-11-17 11:46:34 +0200

    tsüklitetöö

---
+++ Tsüklitetöö/Program.cs
@@ -0,0 +1,27 @@
+﻿//1. ülesanne
+string kasutajanimi = "";
+int parool = 0;
+if ( kasutajanimi == "")
+{
+    do
+    {
+        Console.WriteLine("Mis on sinu nimi?");
+        kasutajanimi = Console.ReadLine();
+    } while (kasutajanimi == "");
+}
+
+
+while (parool == 0)
+{
+    Console.WriteLine("Mis on sinu parool?");
+    parool = int.Parse(Console.ReadLine());
+    do
+    {
+        Console.WriteLine("parool peab olema neljakohaline");
+    } while (parool < 1000 && parool > 9999);
+}
+string[] puuviljad = new string[5] {"a", "l", "p", "g", "f"}; 
+for (int i = 0; i < puuviljad.Length; i++)
+{
+    Console.WriteLine("");
+}
